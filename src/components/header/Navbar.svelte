d75fc683bb27144e7d13574d7c9c382ec6e6c5bb
Chritie MPASSI <christmp19@gmail.com>
2024-06-04 12:49:32 +0100

    feat: Header compoment

--- src/components/header/Navbar.svelte
+++ src/components/header/Navbar.svelte
@@ -8,44 +8,77 @@
 		Button,
 	} from "flowbite-svelte";
 	import { DarkMode } from "flowbite-svelte";
-	import { CalendarWeekSolid } from "flowbite-svelte-icons";
+	import { ShoppingBagSolid } from "flowbite-svelte-icons";
+	import Logo from "../../assets/logo.png";
+    import { onMount } from "svelte";
 	let btnClass =
-		"text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg text-xl p-2";
+		'text-black dark:text-white dark:bg-transparent bg-transparent hover:bg-gray-200 dark:hover:bg-gray-600 rounded-lg text-xl p-1';
+	let isScrolled = false;
+	let activeSection = 'home';
+	function handleScroll() {
+		isScrolled = window.scrollY > 100;
+	}
+
+	// @ts-ignore
+	function updateActiveSection(entries) {
+		// @ts-ignore
+		entries.forEach((entry) => {
+			if (entry.isIntersecting && entry.target.id !== activeSection) {
+				activeSection = entry.target.id;
+			}
+		});
+	}
+
+	onMount(() => {
+		window.addEventListener('scroll', handleScroll);
+
+		const observer = new IntersectionObserver(updateActiveSection, {
+			threshold: 0.5
+		});
+
+		document.querySelectorAll('section').forEach((section) => {
+			observer.observe(section);
+		});
+
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+			observer.disconnect();
+		};
+	});
 
 </script>
-
-<Navbar fluid class="gap-20 dark:bg-slate-700 ">
-	<div class="flex md:order-2">
-		<div class="flex items-center sm:hidden">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="1.5em"
-				height="1.5em"
-				viewBox="0 0 256 256"
-				{...$$props}
-			>
-				<path
-					fill="currentColor"
-					d="M231.88 175.08A56.26 56.26 0 0 1 176 224C96.6 224 32 159.4 32 80a56.26 56.26 0 0 1 48.92-55.88a16 16 0 0 1 16.62 9.52l21.12 47.15v.12A16 16 0 0 1 117.39 96c-.18.27-.37.52-.57.77L96 121.45c7.49 15.22 23.41 31 38.83 38.51l24.34-20.71a8 8 0 0 1 .75-.56a16 16 0 0 1 15.17-1.4l.13.06l47.11 21.11a16 16 0 0 1 9.55 16.62"
-				/>
-			</svg>
-		</div>
+ 
+<nav class='dark:bg-gray-800'>
+<Navbar fluid class="dark:bg-gray-800 container mx-auto lg:max-w-6xl md:max-w-4xl max-w-2xl md:px-8 px-4">
+	<div class=" md:order-3 order-2 flex">
+		<DarkMode {btnClass} />
 		<NavHamburger
-			menuClass={"outline-none bg-transparent p-1 sm:ml-2 hover:bg-transparent sm:hover:bg-gray-700"}
+			class={"outline-none text-black dark:text-white dark:bg-transparent bg-transparent hover:bg-gray-200 dark:hover:bg-gray-600 rounded-lg text-xl p-1"}
 		/>
-		<div class="-ml-2 sm:ml-2 flex">
-			<DarkMode {btnClass} />
-		</div>
+	
 	</div>
 
-	<NavUl class="order-2 xl:order-1 font-['Inter']">
-		<NavLi href="#about" id="anime">À propos de nous</NavLi>
-		<NavLi href="#services" id="anime">Services</NavLi>
-		<NavLi href="#blog" id="anime">Blog</NavLi>
-		<NavLi href="#caseStudies" id="anime">Etudes de cas</NavLi>
-		<NavLi href="#howItWorks" id="anime">Comment ça fonctionne</NavLi>
+	<NavUl class=" order-2 font-['Inter']">
+		<NavLi href="#home" id="anime" class={`text-lg dark:text-white ${activeSection === 'home' ? 'active' : ''}`}>Accueil</NavLi>
+		<NavLi href="#about" id="anime" class={`text-lg dark:text-white ${activeSection === 'about' ? 'active' : ''}`}>A propos</NavLi>
+		<NavLi href="#popularMenue" id="anime" class={`text-lg dark:text-white ${activeSection === 'popularMenue' ? 'active' : ''}`}>Menu populaire</NavLi>
+		<NavLi href="#menu" id="anime" class={`text-lg dark:text-white ${activeSection === 'menu' ? 'active' : ''}`}>Menu</NavLi>
+		<NavLi href="#order" id="anime" class={`text-lg dark:text-white ${activeSection === 'order' ? 'active' : ''}`}>Commander</NavLi>
+		<NavLi href="#blog" id="anime" class={`text-lg dark:text-white ${activeSection === 'panier' ? 'active' : ''}`}>Blog</NavLi>
+		<NavLi href="#panier" id="anime" class='flex'>
+			<ShoppingBagSolid class='w-8 md:w-6 md:h-6 h-8' />
+			<span class='absolute md:top-5 ml-6 md:ml-4 w-5 h-5 md:w-4 md:h-4 rounded-full flex justify-center items-center bg-green-600 text-center text-white'>0</span>
+		 </NavLi>
 		<!-- <NavLi href="#recrutement">Recrutement</NavLi> -->
 	</NavUl>
-	<CalendarWeekSolid />
+       <div class='order-1'>
+		 <a href="/" class="flex items-center gap-2"> 
+			<img src="{Logo}" alt="logo" class='w-6 h-6'>
+			<span class='capitalize text-2xl font-bold text-blue-950 dark:text-white'>food</span>
+		</a>
+	   </div>
+	
 </Navbar>
+</nav>
+
 <!-- <h1 class="animate__animated animate__bounce">An animated element</h1> -->
