499cd94ad54da48d1cd024b6fd7478e236a5b013
Chritie MPASSI <christmp19@gmail.com>
2024-06-12 11:09:18 +0100

    feat: add a cover

--- src/components/404.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    export let location;
-</script>
-
-<div>
-    <h1>404 - Page Not Found</h1>
-    <p>The page at {location} does not exist.</p>
-    <a href="/">Back to Home</a>
-</div>
